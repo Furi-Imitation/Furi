2c1c161af0ba35bab30e0ae4c90dd9af21a4f92f
ssr5087 <ssr5087@gmail.com>
2026-03-27 14:59:16 +0900

    0327 1458 SSR
    대쉬 구현
    Cue에서 VFX, Sound 구현 필요

---
+++ Source/Furi/SSR/Skill/SSRDash.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "SSRDash.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FURI_API USSRDash : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	USSRDash();
+	
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	FVector GetDashDirection() const;
+
+protected:
+	UFUNCTION()
+	void OnDashFinished();
+	
+	UFUNCTION()
+	FVector GetDashDirection();
+	
+	UPROPERTY(EditAnywhere, Category = "Dash")
+	float DashStrength = 2000.f;
+	
+	UPROPERTY(EditAnywhere, Category = "Dash")
+	float DashDuration = 0.3f;
+	
+protected:
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+	
+	
+};
