36231d4f3c0c63d791b108852deff2e2db2f3a51
jun <50354282+wjdwns@users.noreply.github.com>
2026-03-30 15:34:31 +0900

    데미지 적용 로직 수정

--- Source/Furi/SSR/Skill/SSRAttack.cpp
+++ Source/Furi/SSR/Skill/SSRAttack.cpp
@@ -13,170 +13,211 @@
 
 USSRAttack::USSRAttack()
 {
-    InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
-    NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
-    ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Lock")));
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+	ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Lock")));
 }
 
-void USSRAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+void USSRAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                                 const FGameplayAbilityActivationInfo ActivationInfo,
+                                 const FGameplayEventData* TriggerEventData)
 {
-    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-    
-    if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
-    {
-        EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
-        return;
-    }
-    
-    CurrentComboIndex = 4;
-    bComboWindowOpened = false;
-    bNextComboReserved = false;
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-    UAbilityTask_WaitGameplayEvent* WaitStartTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, FGameplayTag::RequestGameplayTag(FName("Combo.Start")));
-    WaitStartTask->EventReceived.AddDynamic(this, &USSRAttack::OnComboEventReceived);
-    WaitStartTask->ReadyForActivation();
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
 
-    UAbilityTask_WaitGameplayEvent* WaitEndTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, FGameplayTag::RequestGameplayTag(FName("Combo.End")));
-    WaitEndTask->EventReceived.AddDynamic(this, &USSRAttack::OnComboEventReceived);
-    WaitEndTask->ReadyForActivation();
-    
-    UAbilityTask_WaitGameplayEvent* HitCheckTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(this, HitCheckEventTag);
-    HitCheckTask->EventReceived.AddDynamic(this, &USSRAttack::OnComboEventReceived);
-    HitCheckTask->ReadyForActivation();
-    
-    if (AActor* MyAvatar = GetAvatarActorFromActorInfo())
-    {
-        RotateTowardsClosestEnemy(MyAvatar, AttackRange);
-    }
-    
-    PlayComboSection();
+	CurrentComboIndex = 4;
+	bComboWindowOpened = false;
+	bNextComboReserved = false;
+
+	UAbilityTask_WaitGameplayEvent* WaitStartTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this, FGameplayTag::RequestGameplayTag(FName("Combo.Start")));
+	WaitStartTask->EventReceived.AddDynamic(this, &USSRAttack::OnComboEventReceived);
+	WaitStartTask->ReadyForActivation();
+
+	UAbilityTask_WaitGameplayEvent* WaitEndTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this, FGameplayTag::RequestGameplayTag(FName("Combo.End")));
+	WaitEndTask->EventReceived.AddDynamic(this, &USSRAttack::OnComboEventReceived);
+	WaitEndTask->ReadyForActivation();
+
+	UAbilityTask_WaitGameplayEvent* HitCheckTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this, HitCheckEventTag);
+	HitCheckTask->EventReceived.AddDynamic(this, &USSRAttack::OnComboEventReceived);
+	HitCheckTask->ReadyForActivation();
+
+	if (AActor* MyAvatar = GetAvatarActorFromActorInfo())
+	{
+		RotateTowardsClosestEnemy(MyAvatar, AttackRange);
+	}
+
+	PlayComboSection();
 }
 
-void USSRAttack::InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+void USSRAttack::InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+                              const FGameplayAbilityActivationInfo ActivationInfo)
 {
-    Super::InputPressed(Handle, ActorInfo, ActivationInfo);
+	Super::InputPressed(Handle, ActorInfo, ActivationInfo);
 
-    if (bNextComboReserved) return; 
+	if (bNextComboReserved)
+	{
+		return;
+	}
 
-    if (bComboWindowOpened && CurrentComboIndex < 7)
-    {
-        bNextComboReserved = true; 
-        FName CurrentSection = *FString::Printf(TEXT("Attack%d"), CurrentComboIndex);
-        FName NextSection = *FString::Printf(TEXT("Attack%d"), CurrentComboIndex + 1);
-        MontageSetNextSectionName(CurrentSection, NextSection);
-        CurrentComboIndex++; 
-    }
+	if (bComboWindowOpened && CurrentComboIndex < 7)
+	{
+		bNextComboReserved = true;
+		FName CurrentSection = *FString::Printf(TEXT("Attack%d"), CurrentComboIndex);
+		FName NextSection = *FString::Printf(TEXT("Attack%d"), CurrentComboIndex + 1);
+		MontageSetNextSectionName(CurrentSection, NextSection);
+		CurrentComboIndex++;
+	}
 }
 
 void USSRAttack::OnComboEventReceived(FGameplayEventData Payload)
 {
-    FGameplayTag EventTag = Payload.EventTag;
+	FGameplayTag EventTag = Payload.EventTag;
 
-    if (EventTag.MatchesTag(FGameplayTag::RequestGameplayTag(FName("Combo.Start"))))
-    {
-        bComboWindowOpened = true;
-        bNextComboReserved = false; 
-    }
-    else if (EventTag.MatchesTag(FGameplayTag::RequestGameplayTag(FName("Combo.End"))))
-    {
-        bComboWindowOpened = false;
-    }
-    else if (EventTag.MatchesTag(HitCheckEventTag))
-    {
-        PerformHitCheck();
-    }
+	if (EventTag.MatchesTag(FGameplayTag::RequestGameplayTag(FName("Combo.Start"))))
+	{
+		bComboWindowOpened = true;
+		bNextComboReserved = false;
+	}
+	else if (EventTag.MatchesTag(FGameplayTag::RequestGameplayTag(FName("Combo.End"))))
+	{
+		bComboWindowOpened = false;
+	}
+	else if (EventTag.MatchesTag(HitCheckEventTag))
+	{
+		PerformHitCheck();
+	}
 }
 
 void USSRAttack::PlayComboSection()
 {
-    if (!ComboMontage) return;
+	if (!ComboMontage)
+	{
+		return;
+	}
 
-    FName SectionName = *FString::Printf(TEXT("Attack%d"), CurrentComboIndex);
-    MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("AttackTask"), ComboMontage, 1.0f, SectionName);
-    if (MontageTask)
-    {
-        MontageTask->OnCompleted.AddDynamic(this, &USSRAttack::OnMontageCompleted);
-        MontageTask->OnInterrupted.AddDynamic(this, &USSRAttack::OnMontageCompleted);
-        MontageTask->ReadyForActivation();
-    }
+	FName SectionName = *FString::Printf(TEXT("Attack%d"), CurrentComboIndex);
+	MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+		this, TEXT("AttackTask"), ComboMontage, 1.0f, SectionName);
+	if (MontageTask)
+	{
+		MontageTask->OnCompleted.AddDynamic(this, &USSRAttack::OnMontageCompleted);
+		MontageTask->OnInterrupted.AddDynamic(this, &USSRAttack::OnMontageCompleted);
+		MontageTask->ReadyForActivation();
+	}
 }
 
 void USSRAttack::PerformHitCheck()
 {
-    AActor* MyAvatar = GetAvatarActorFromActorInfo();
-    AGasCharacterBase* MyCharacter = Cast<AGasCharacterBase>(MyAvatar);
-    if (!MyCharacter) return;
+	AActor* MyAvatar = GetAvatarActorFromActorInfo();
+	AGasCharacterBase* MyCharacter = Cast<AGasCharacterBase>(MyAvatar);
+	if (!MyCharacter)
+	{
+		return;
+	}
 
-    int32 ActualIndex = CurrentComboIndex;
-    UAnimInstance* AnimInstance = MyCharacter->GetMesh()->GetAnimInstance();
-    if (AnimInstance)
-    {
-        FName CurrentSection = AnimInstance->Montage_GetCurrentSection(ComboMontage);
-        FString SectionStr = CurrentSection.ToString();
-        if (SectionStr.Contains(TEXT("4"))) ActualIndex = 4;
-        else if (SectionStr.Contains(TEXT("5"))) ActualIndex = 5;
-        else if (SectionStr.Contains(TEXT("6"))) ActualIndex = 6;
-        else if (SectionStr.Contains(TEXT("7"))) ActualIndex = 7;
-    }
+	int32 ActualIndex = CurrentComboIndex;
+	UAnimInstance* AnimInstance = MyCharacter->GetMesh()->GetAnimInstance();
+	if (AnimInstance)
+	{
+		FName CurrentSection = AnimInstance->Montage_GetCurrentSection(ComboMontage);
+		FString SectionStr = CurrentSection.ToString();
+		if (SectionStr.Contains(TEXT("4")))
+		{
+			ActualIndex = 4;
+		}
+		else if (SectionStr.Contains(TEXT("5")))
+		{
+			ActualIndex = 5;
+		}
+		else if (SectionStr.Contains(TEXT("6")))
+		{
+			ActualIndex = 6;
+		}
+		else if (SectionStr.Contains(TEXT("7")))
+		{
+			ActualIndex = 7;
+		}
+	}
 
-    FVector Forward = MyAvatar->GetActorForwardVector();
-    FVector BoxHalfExtent = FVector(150.f, 100.f, 100.f);
-    FVector BoxCenter = MyAvatar->GetActorLocation() + (Forward * 150.f);
+	FVector Forward = MyAvatar->GetActorForwardVector();
+	FVector BoxHalfExtent = FVector(150.f, 100.f, 100.f);
+	FVector BoxCenter = MyAvatar->GetActorLocation() + (Forward * 150.f);
 
-    TArray<FOverlapResult> Overlaps;
-    FCollisionQueryParams Params;
-    Params.AddIgnoredActor(MyAvatar);
-    
-    bool bHit = GetWorld()->OverlapMultiByChannel(Overlaps, BoxCenter, MyAvatar->GetActorRotation().Quaternion(), ECC_Pawn, FCollisionShape::MakeBox(BoxHalfExtent), Params);
+	TArray<FOverlapResult> Overlaps;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(MyAvatar);
 
-    if (bHit)
-    {
-        TArray<AActor*> HitActors;
-        for (auto& Result : Overlaps)
-        {
-            AActor* OverlappedActor = Result.GetActor();
-            if (!OverlappedActor || HitActors.Contains(OverlappedActor)) continue;
-            
-            if (AGasCharacterBase* Target = Cast<AGasCharacterBase>(OverlappedActor))
-            {
-                HitActors.Add(OverlappedActor);
-                
-                FFuriDamageInfo DamageInfo;
-                DamageInfo.Amount = (ActualIndex == 7) ? -30.f : -10.f;
-                DamageInfo.DamageResponse = (ActualIndex == 7) ? EFuriDamageResponse::KnockBack : EFuriDamageResponse::HitReaction;
-                DamageInfo.bCanBeParried = (ActualIndex < 7);
-                DamageInfo.bCanBeBlocked = true;
+	bool bHit = GetWorld()->OverlapMultiByChannel(Overlaps, BoxCenter, MyAvatar->GetActorRotation().Quaternion(),
+	                                              ECC_Pawn, FCollisionShape::MakeBox(BoxHalfExtent), Params);
 
-                TSubclassOf<UGameplayEffect> DamageGE = ComboDamageMap.Contains(ActualIndex) ? ComboDamageMap[ActualIndex] : nullptr;
-                if (DamageGE)
-                {
-                    FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponentFromActorInfo()->MakeEffectContext();
-                    FFuriGameplayEffectContext* FuriContext = FFuriGameplayEffectContext::GetFuriContext(ContextHandle);
-                    if (FuriContext)
-                    {
-                        FuriContext->SetDamageInfo(DamageInfo);
-                    }
+	if (bHit)
+	{
+		TArray<AActor*> HitActors;
+		for (auto& Result : Overlaps)
+		{
+			AActor* OverlappedActor = Result.GetActor();
+			if (!OverlappedActor || HitActors.Contains(OverlappedActor))
+			{
+				continue;
+			}
 
-                    FGameplayEffectSpecHandle Spec = MakeOutgoingGameplayEffectSpec(DamageGE);
-                    if (Spec.IsValid())
-                    {
-                        Spec.Data.Get()->SetContext(ContextHandle);
-                        Spec.Data.Get()->SetSetByCallerMagnitude(FGameplayTag::RequestGameplayTag(FName("Data.Damage.Amount")), DamageInfo.Amount);
-                        Target->GetAbilitySystemComponent()->ApplyGameplayEffectSpecToSelf(*Spec.Data.Get());
-                    }
-                }
-            }
-        }
-    }
-    
+			if (AGasCharacterBase* Target = Cast<AGasCharacterBase>(OverlappedActor))
+			{
+				HitActors.Add(OverlappedActor);
+
+				FFuriDamageInfo DamageInfo;
+				DamageInfo.Amount = (ActualIndex == 7) ? -30.f : -10.f;
+				DamageInfo.DamageResponse = (ActualIndex == 7)
+					                            ? EFuriDamageResponse::KnockBack
+					                            : EFuriDamageResponse::HitReaction;
+				DamageInfo.bCanBeParried = (ActualIndex < 7);
+				DamageInfo.bCanBeBlocked = true;
+
+				TSubclassOf<UGameplayEffect> DamageGE = ComboDamageMap.Contains(ActualIndex)
+					                                        ? ComboDamageMap[ActualIndex]
+					                                        : nullptr;
+				if (DamageGE)
+				{
+					FGameplayEffectContextHandle ContextHandle = FGameplayEffectContextHandle(
+						new FFuriGameplayEffectContext());
+					ContextHandle.AddInstigator(MyAvatar, MyAvatar);
+
+					FFuriGameplayEffectContext* FuriContext = FFuriGameplayEffectContext::GetFuriContext(ContextHandle);
+					if (FuriContext)
+					{
+						FuriContext->SetDamageInfo(DamageInfo);
+					}
+
+					FGameplayEffectSpecHandle Spec = MakeOutgoingGameplayEffectSpec(DamageGE);
+					if (Spec.IsValid())
+					{
+						Spec.Data.Get()->SetContext(ContextHandle);
+						Spec.Data.Get()->SetSetByCallerMagnitude(
+							FGameplayTag::RequestGameplayTag(FName("Data.Damage.Amount")), DamageInfo.Amount);
+						Target->GetAbilitySystemComponent()->ApplyGameplayEffectSpecToSelf(*Spec.Data.Get());
+					}
+				}
+			}
+		}
+	}
+
 #if !UE_BUILD_SHIPPING
-    DrawDebugBox(GetWorld(), BoxCenter, BoxHalfExtent, MyAvatar->GetActorRotation().Quaternion(), FColor::Red, false, 2.0f, 0, 2.0f);
+	DrawDebugBox(GetWorld(), BoxCenter, BoxHalfExtent, MyAvatar->GetActorRotation().Quaternion(), FColor::Red, false,
+	             2.0f, 0, 2.0f);
 #endif
 }
 
 void USSRAttack::OnMontageCompleted()
 {
-    EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
 }
 
 void USSRAttack::RotateTowardsClosestEnemy(AActor* MyAvatar, float SearchRadius)
