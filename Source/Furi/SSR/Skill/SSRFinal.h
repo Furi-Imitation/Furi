23412755f007cc5690ddf21401396459705c2d4f
ssr5087 <ssr5087@gmail.com>
2026-04-01 18:01:44 +0900

    04011801 SSR
    필살기 애니메이션 따는중

---
+++ Source/Furi/SSR/Skill/SSRFinal.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "SSRFinal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FURI_API USSRFinal : public UGameplayAbility
+{
+	GENERATED_BODY()
+};
